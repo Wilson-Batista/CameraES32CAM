c5ec6beb2d82b15f2daf4b406258dde3dd398a87
wilson <wilsonescorpiao7@gmail.com>
2024-01-08 00:09:08 -0300

    Aplicação atualizado e revisado

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# CameraES32CAM
-Projeto Câmera ESP32CAM
+## CameraES32CAM
+# Dependencias da aplicação
+CameraIP
+1. Flask: Uma estrutura da web em Python para execução do script index.html
+```
+pip install Flask
+```
+<<<<<<< HEAD
+2. OpenCV (cv2): Uma biblioteca de visão computacional em código aberto que possibilita capturar, processar e analisar video em tempo real.
+```
+=======
+```OpenCV (cv2): Uma biblioteca de visão computacional em código aberto
+>>>>>>> parent of f537dfc (Update README.md)
+pip install opencv-python
+```
+3. NumPy: Uma biblioteca que possibilita a realização de operaçoes matematicas em arrays multidimensionais, utilizado para converter os bytes dos flames em um array do tipo de 8 bits.
+```
+pip install numpy
+```
+4. ImageIO: Uma biblioteca que possibilita ler, gravar imagens e videos em varios formatos com o proposito de ser simples sem se preocupar com o formato do arquivo.
+```
+pip install imageio
+```
+5. Ngrok: E uma ferramenta que possibilita o acesso publico a servidores ou serviço locais mesmo estando tras de forewals ou roteadores, fornecendo links url publico que redireciona o trafego ao servidor local.
+Baixar o Ngrok ubuntu.
+```
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+
+```
+6. descompactar Ngrok.
+```
+unzip ngrok-stable-linux-amd64.zip
+
+```
+7. Movendo o executavel e sua variavel para pasta bin
+```
+sudo mv ngrok /usr/local/bin/
+```
+8. Execução da aplicação
+```
+ngrok http 5000
+```
---
+++ esp32cam.py
@@ -0,0 +1,87 @@
+from flask import Flask, Response, render_template, request, jsonify
+import cv2
+import numpy as np
+import threading
+import time
+import imageio
+from queue import Queue
+import os  # Importe a biblioteca os para manipulação de arquivos e diretórios
+
+app = Flask(__name__)
+
+# Variáveis de controle de gravação
+recording = False
+recording_frames = Queue()
+
+def gen_frames():
+    global recording_frames
+    cap = cv2.VideoCapture("http://192.168.2.151:81/stream", cv2.CAP_FFMPEG)
+    cap.set(cv2.CAP_PROP_FPS, 24)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)  # Ajuste o tamanho do buffer de vídeo
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Sem frame de vídeo")
+            break
+
+        # Codifique o frame como JPEG
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        # Se estiver gravando, adicione o frame à fila
+        if recording:
+            recording_frames.put(frame)
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/start_recording', methods=['POST'])
+def start_recording():
+    global recording, recording_frames
+    recording = True
+    recording_frames = Queue()  # Limpa a fila quando inicia a gravação
+    return jsonify({'status': 'Recording started'})
+
+@app.route('/stop_recording', methods=['POST'])
+def stop_recording():
+    global recording
+    recording = False
+    return jsonify({'status': 'Recording stopped'})
+
+@app.route('/download_recording')
+def download_recording():
+    if recording_frames.qsize() > 0:
+        # Crie o diretório 'static' se não existir
+        directory = 'static'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Crie o arquivo de vídeo usando a biblioteca imageio
+        video_path = 'static/recording.mp4'
+        writer = imageio.get_writer(video_path, fps=24)
+
+        while not recording_frames.empty():
+            frame = np.frombuffer(recording_frames.get(), dtype=np.uint8)
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            writer.append_data(frame)
+
+        writer.close()
+
+        return jsonify({'status': 'Recording downloaded'})
+    else:
+        return jsonify({'status': 'No frames to download'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
